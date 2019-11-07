e2ec2eb27523763df334596145f8919568a7f05e
Limpat Prananda <limpat.prananda@gmail.com>
2019-11-07 14:51:55 +0700

    return when many conditional situation

--- src/AMS.kt
+++ src/AMS.kt
@@ -6,10 +6,29 @@ fun main(args: Array<String>){
     feedTheFish()
 }
 
+fun shouldChangedWater(
+    day: String,
+    temperature: Int = 22,
+    dirty: Int = 20
+): Boolean{
+    return when{
+        temperature > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" -> true
+        else -> false
+    }
+}
+
 fun feedTheFish(){
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
+    shouldChangedWater(day, 50, 30)
+    shouldChangedWater(day)
+
+    if(shouldChangedWater(day)){
+        println("Change the water today")
+    }
 }
 
 fun fishFood(day: String): String {
