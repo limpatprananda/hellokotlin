ab1c76b30fb8f9356cd4591cb5e762131354c995
Limpat Prananda <limpat.prananda@gmail.com>
2019-11-07 15:26:20 +0700

    list operating

--- src/AMS.kt
+++ src/AMS.kt
@@ -2,14 +2,42 @@ import java.util.*
 
 fun main(args: Array<String>){
     println("Hello ${args[0]}")
-
     feedTheFish()
+
+    eagerExample()
+}
+
+fun eagerExample() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "aligator", "flowerplant")
+
+    val eager = decorations.filter { it[0] == 'p'}
+    println(eager)
+
+    //apply filter lazily
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println(filtered)
+    println(filtered.toList())
+
+    //apply map lazily
+    val lazyMap = decorations.asSequence().map {
+        println("map: $it")
+        it
+    }
+    println(lazyMap)
+    println("first: ${lazyMap.first()}")
+    println("all: ${lazyMap.toList()}")
+}
+
+fun repeatSwimming(times: Int = 2){
+    repeat(times){
+        println("A fish is swimming $it")
+    }
 }
 
 fun shouldChangedWater(
     day: String,
     temperature: Int = 22,
-    dirty: Int = 20
+    dirty: Int = getDirtySensorReading()
 ): Boolean{
 
     return when{
@@ -23,6 +51,12 @@ fun shouldChangedWater(
 fun isTooHot(temperature: Int) = temperature > 30
 fun isDirty(dirty: Int) = dirty > 30
 fun isSunday(day: String) = day == "Sunday"
+fun getDirtySensorReading() = 20
+fun makeNewAquarium() = println("Building new aquarium...")
+
+fun aquariumStatusReport(aquarium: Any = makeNewAquarium()){
+
+}
 
 fun feedTheFish(){
     val day = randomDay()
